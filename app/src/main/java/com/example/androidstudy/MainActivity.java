9a50d81c610063b0b28ee0c74357b3c9043c878a
MochiNo3 <tyk7481@naver.com>
2020-07-04 22:58:19 +1000

    Android:LinearLayout study

--- app/src/main/java/com/example/androidstudy/MainActivity.java
+++ app/src/main/java/com/example/androidstudy/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.androidstudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int abs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        abs = 1     ;
+        abs = 2;
+        init();
+    }
+
+    private void init() {
+        abs = 4;
     }
 }
