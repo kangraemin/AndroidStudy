f762d669cd75191aa140be85faa1e45dc9483c6b
MochiNo3 <tyk7481@naver.com>
2020-07-08 00:34:56 +1000

    Making LoginScreen01

--- app/src/main/java/com/example/androidstudy/MainActivity.java
+++ app/src/main/java/com/example/androidstudy/MainActivity.java
@@ -1,25 +1,15 @@
 package com.example.androidstudy;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
-    private int abs;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        abs = 1     ;
-        abs = 2;
-        init();
-    }
-
-    private void init() {
-        abs = 4;
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_login);
+        }
 }
