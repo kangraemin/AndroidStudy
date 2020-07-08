33f07bc6bd9aae590be8b72a298763feea11a1f8
Kangraemin <ee4787@naver.com>
2020-07-08 11:40:29 +0900

    Revert "Add LinearLayout"

--- app/src/main/java/com/example/androidstudy/MainActivity.java
+++ app/src/main/java/com/example/androidstudy/MainActivity.java
@@ -1,15 +1,14 @@
 package com.example.androidstudy;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_login);
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
